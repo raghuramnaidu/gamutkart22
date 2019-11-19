aaa81b9a2b26a96dfceb25ad3a44072adaf51d4a
raghuramnaidu <prrchinna73@gmail.com>
2019-11-19 12:49:56 +0530

    another file added

---
+++ New.java
@@ -0,0 +1,11 @@
+public class New
+{
+	public static void main (String[] args)
+	{
+		int a=5,b=5,c;
+		System.out.println("print a, b values"+a +b);
+		c=a+b;
+		System.out.println("addition of a,b is"+c);
+	}
+}
+		
